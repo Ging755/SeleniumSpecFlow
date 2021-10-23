0edd76edc3c9dd8175d203db019a511f74a5548e
Denis <denis.milic2@gmail.com>
2021-10-23 23:11:03 +0200

    no message

---
+++ SpecFlowSelenium/SpecFlowSelenium/PageObjects/ProductPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecFlowSelenium.PageObjects
+{
+    class ProductPage
+    {
+        private IWebDriver _driver;
+
+        public ProductPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public List<IWebElement> ProductItems => _driver.FindElements(By.ClassName("inventory_item")).ToList();
+        public IWebElement AddToCartOrRemoveFromCartButton => ProductItems.FirstOrDefault().FindElement(By.XPath(".//button"));
+        public IWebElement FirstProductItemPrice => ProductItems.FirstOrDefault().FindElement(By.XPath(".//div[@class='inventory_item_price']"));
+        public IWebElement CartElement => _driver.FindElement(By.ClassName("shopping_cart_link"));
+        public SelectElement FillterDropDown => new SelectElement(_driver.FindElement(By.ClassName("product_sort_container")));
+
+        public IWebElement CartCount()
+        {
+            try
+            {
+                return CartElement.FindElement(By.ClassName("shopping_cart_badge"));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SpecFlowSelenium/SpecFlowSelenium/StepDefinitions/ProductFeatureSteps.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using SpecFlowSelenium.Context;
+using SpecFlowSelenium.PageObjects;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSelenium.StepDefinitions
+{
+    [Binding]
+    public class ProductFeatureSteps : IDisposable
+    {
+        private LoginPage _loginPage;
+        private ProductPage _productPage;
+        private ChromeDriver _driver;
+
+        public ProductFeatureSteps(WebDriverContext webDriverContext)
+        {
+            _driver = webDriverContext.driver;
+        }
+
+        [Given(@"I have logged in successfully with (.*) username and (.*) password")]
+        public void GivenIHaveLoggedInSuccessfullyWithStandard_UserUsernameAndSecret_SaucePassword(string username, string password)
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            _loginPage = new LoginPage(_driver);
+            _productPage = new ProductPage(_driver);
+            _loginPage.UserName.SendKeys(username);
+            _loginPage.Password.SendKeys(password);
+            _loginPage.LoginButton.Submit();
+            Assert.IsTrue(true);
+        }
+        
+        [When(@"I press add to cart or remove from cart button")]
+        public void WhenIPressAddToCartOrRemoveFromCartButton()
+        {
+            _productPage.AddToCartOrRemoveFromCartButton.Click();
+        }
+        
+        [When(@"I select ""(.*)""")]
+        public void WhenISelect(string dropdownText)
+        {
+            _productPage.FillterDropDown.SelectByText(dropdownText);
+        }
+        
+        [Then(@"any number of product items should be displayed")]
+        public void ThenAnyNumberOfProductItemsShouldBeDisplayed()
+        {
+            Assert.IsTrue(_productPage.ProductItems.Count > 0);
+        }
+        
+        [Then(@"product item should be added the cart and (.*) button should be displayed")]
+        public void ThenProductItemShouldBeAddedTheCartAndRemoveFromCartButtonShouldBeDisplayed(string buttonText)
+        {
+            Assert.IsTrue(_productPage.AddToCartOrRemoveFromCartButton.Text.Equals(buttonText));
+            Assert.IsTrue(Int32.Parse(_productPage.CartCount().Text) > 0);
+        }
+        
+        [Then(@"product item should be removed from the cart and (.*) button should be displayed")]
+        public void ThenProductItemShouldBeRemovedFromTheCartAndAddToCartButtonShouldBeDisplayed(string buttonText)
+        {
+            Assert.IsTrue(_productPage.AddToCartOrRemoveFromCartButton.Text.Equals(buttonText));
+            Assert.IsTrue(_productPage.CartCount() == null);
+        }     
+
+        [Then(@"first item should be priced (.*)")]
+        public void ThenFirstItemShouldBePriced(string itemPrice)
+        {
+            Assert.IsTrue(_productPage.FirstProductItemPrice.Text.Equals(itemPrice));
+        }
+
+        public void Dispose()
+        {
+            if (_driver != null)
+            {
+                _driver.Dispose();
+                _driver = null;
+            }
+        }
+    }
+}
